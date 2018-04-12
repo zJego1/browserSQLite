e4c017de5d33492e321f98bf51d3db0396f12bd8
zJego1 <38275461+zJego1@users.noreply.github.com>
2018-04-12 09:39:28 +0000

    Update webServer.cpp

--- webServer.cpp
+++ webServer.cpp
@@ -1,11 +1,1 @@
-tag con NOME DB e SELECT
-restituisce una tabella
-costruiamo una tabella HTML
 
-Nostro tag: <sql db="scuola" query="SELECT * FROM Studenti WHERE...">
-
-quando trova quello da spedire al client crea un file nuovo e ci scrive una table con il risultato
-query restituisce un elenco di righe, su cui facciamo una fetch, chiudiamo.
-
-Oltre a mandare la pagina HTML il webServer manda anche un file con il risultato della query,
-che sarà da formattare in HTML.
