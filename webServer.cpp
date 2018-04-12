5719c0a75a594c0ad081b3c6d6f8f4c9e816f9a0
zJego1 <38275461+zJego1@users.noreply.github.com>
2018-04-12 09:56:52 +0000

    Update webServer.cpp

--- webServer.cpp
+++ webServer.cpp
@@ -1,5 +1,18 @@
+//<sql db="scuola" query="SELECT * FROM Studenti WHERE...">
+#include "TCP.hpp"
+#include "errore.h"
+#include <string.h>
+#define TAGSCUOLA "sql"
 
-
-int main(){
-  
+int main(int argc, char* argv[]){
+	if(argc!=3){
+		printf("USAGE: %s PORT ROOT", argv[0]);
+		return -1;
+	}
+	int port = atoi(argv[1]);
+	char* root = argv[2];
+	
+	ServerTCP myself(port, true);
+	Connection* connection = myself.accetta();
+	
 }
